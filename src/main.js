499b52cc3c5ecab5558630c85f2b93107b463085
HuangKai <mail.huangkai@gmail.com>
2023-02-25 00:43:58 +0800

    设置服务端响应状态码、在客户端的请求里设置头部数据、在服务端设置响应的头部数据

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/**
+ * 使用 JSON 中间件
+ */
+app.use(express.json());
+
 app.listen(port, () => {
     console.log('🚀 服务已启动！');
 });
@@ -40,4 +45,26 @@ app.get('/posts/:postId', (request, response) => {
     const posts = data.filter(item => item.id == postId);
     // 做出响应
     response.send(posts[0]);
+});
+
+/**
+ * 创建内容
+ */
+app.post('/posts', (request, response) => {
+    // 获取请求里的数据
+    const { content } = request.body;
+
+    // 设置响应状态码
+    response.status(201);
+
+    // 输出请求头部数据
+    console.log(request.headers['sing-along']);
+
+    // 设置响应数据
+    response.set('Sing-Along', 'How I wonder waht you are!');
+
+    // 作出响应
+    response.send({
+        message: `成功创建了内容：${content}`
+    });
 });
