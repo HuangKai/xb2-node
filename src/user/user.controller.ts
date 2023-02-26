23e306c90829cdc1ce30f3658276fe8a20e5c470
HuangKai <mail.huangkai@gmail.com>
2023-02-26 14:03:43 +0800

    用户模块、定义创建用户的额接口、测试创建用户的接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+import { connection } from '../app/database/mysql';
 import { UserModel } from './user.model';
 import * as userService from './user.service';
+
+/**
+ * 创建用户
+ */
+export const store = async (
+    requst: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    // 准备数据
+    const { name, password } = requst.body;
+
+    // 创建用户
+    try {
+        const data = await userService.createUser({ name, password });
+        response.status(201).send(data);
+    } catch (error) {
+        next(error);
+    }
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -3,7 +3,10 @@ import * as userController from './user.controller';
 
 const router = express.Router();
 
-
+/**
+ * 创建用户
+ */
+router.post('/users', userController.store);
 
 /**
  * 导出路由
