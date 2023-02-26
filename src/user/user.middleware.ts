9e1522ef6cbcd5f0ae37bb43ab2fba5151c2d4c0
HuangKai <mail.huangkai@gmail.com>
2023-02-26 14:13:44 +0800

    处理创建用户的错误处理

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,2 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+
+/**
+ * 验证用户数据
+ */
+export const validateUserData = async (
+    requst: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    console.log('👦🏻 验证用户数据');
+
+    // 准备数据
+    const { name, password } = requst.body;
+
+    // 验证必填数据
+    if (!name) return next(new Error('NAME_IS_REQUIRED'));
+    if (!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+
+    // 下一步
+    next();
+};
