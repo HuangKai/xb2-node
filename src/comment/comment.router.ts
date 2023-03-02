093c544d6d7abb04b5d8413da975e05d90889cab
HuangKai <mail.huangkai@gmail.com>
2023-03-02 23:17:49 +0800

    测试发表评论接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as commentController from './comment.controller';
 
 const router = express.Router();
+
+/**
+ * 发表评论
+ */
+router.post('/comments', authGuard, commentController.store);
 
 /**
  * 导出路由
