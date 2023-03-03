8796eaee82a97ee59915554992b75dcd0534c557
HuangKai <mail.huangkai@gmail.com>
2023-03-03 17:08:40 +0800

    定义修改评论接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authGuard } from '../auth/auth.middleware';
+import { accessControl, authGuard } from '../auth/auth.middleware';
 import * as commentController from './comment.controller';
 
 const router = express.Router();
@@ -13,6 +13,16 @@ router.post('/comments', authGuard, commentController.store);
  * 回复评论
  */
 router.post('/comments/:commentId/reply', authGuard, commentController.reply);
+
+/**
+ * 修改评论
+ */
+router.patch(
+    '/comments/:commentId',
+    authGuard,
+    accessControl({ possession: true }),
+    commentController.update
+);
 
 /**
  * 导出路由
