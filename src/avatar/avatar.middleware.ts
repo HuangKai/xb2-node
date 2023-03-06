deec1cf7ca75a9c3eeed4ba7172f76d4b2c75b66
HuangKai <mail.huangkai@gmail.com>
2023-03-06 22:33:13 +0800

    调整头像的尺寸

--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import Jimp from 'jimp';
 import multer from 'multer';
 import { fileFilter } from '../file/file.middleware';
 
@@ -19,3 +21,43 @@ const avatarUpload = multer({
  * 文件拦截器
  */
 export const avatarInterceptor = avatarUpload.single('avatar');
+
+/**
+ * 头像处理器
+ */
+export const avatarProcessor = async (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+) => {
+    // 准备文件信息
+    const { file } = request;
+
+    // 准备文件路径
+    const filePath = path.join(file.destination, 'resized', file.fieldname);
+
+    // 处理头像
+    try {
+        // 读取文件
+        const image = await Jimp.read(file.path);
+
+        // 调整尺寸
+        image
+            .cover(256, 256)
+            .quality(85)
+            .write(`${filePath}-large`);
+        image
+            .cover(128, 128)
+            .quality(85)
+            .write(`${filePath}-medium`);
+        image
+            .cover(64, 64)
+            .quality(85)
+            .write(`${filePath}-small`);
+    } catch (error) {
+        next(error);
+    }
+
+    // 下一步
+    next();
+}
