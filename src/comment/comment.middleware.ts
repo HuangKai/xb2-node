c02a0699174fb26f2ae5f7ecfe4de215fe4de284
HuangKai <mail.huangkai@gmail.com>
2023-03-08 15:23:25 +0800

    获取内容的评论列表

--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { stringify } from 'querystring';
 
 /**
  * 过滤器
@@ -8,11 +9,25 @@ export const filter = async (
     response: Response,
     next: NextFunction
 ) => {
+    // 解构查询福
+    const { post, user, action } = request.query;
+
     // 默认的过滤
     request.filter = {
         name: 'default',
         sql: 'comment.parentId IS NULL'
     };
+
+    // 内容的评论
+    if (post && !user && !action) {
+        request.filter = {
+            name: 'postComments',
+            sql: 'comment.parentId IS NULL AND comment.postId = ?',
+            param: post as string,
+            // param: `${post}`,    // 字符模板也可以
+            // param: post.toString()   // toString() 方法转为 string 也以
+        }
+    }
 
     // 下一步
     next();
