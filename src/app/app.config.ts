162cf74e0ecfbf95daa4683e61caa9be6597f71d
HuangKai <mail.huangkai@gmail.com>
2023-03-08 20:18:39 +0800

    改造内容分页功能

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -28,4 +28,4 @@ PUBLIC_KEY = Buffer.from(PUBLIC_KEY, 'base64').toString();
 /**
  * 内容分页
  */
-export const { POST_PER_PAGE } = process.env;
+export const POST_PER_PAGE = parseInt(process.env['POST_PER_PAGE'], 10);
