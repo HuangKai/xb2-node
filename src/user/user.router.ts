c893e5f37938911d61dfe68c8703832cb487af31
HuangKai <mail.huangkai@gmail.com>
2023-03-10 00:41:43 +0800

    定义更新用户接口

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as userController from './user.controller';
-import { hashPassword, validateUserData } from './user.middleware';
+import { hashPassword, validateUserData, validateUpdateUserData } from './user.middleware';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
 
@@ -13,6 +14,11 @@ router.post('/users', validateUserData, hashPassword, userController.store);
  * 用户账户
  */
 router.get('/users/:userId', userController.show);
+
+/**
+ * 更新用户
+ */
+router.patch('/users', authGuard, validateUpdateUserData, userController.update);
 
 /**
  * 导出路由
